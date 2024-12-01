19352886c6a1b3cd0a5b2db881989aadd7adff55
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-12-02 00:00:04 +0900

    save

--- streamlit_study/2_api_basicElementsAndLayouts/Layouts.py
+++ streamlit_study/2_api_basicElementsAndLayouts/Layouts.py
@@ -1,6 +1,17 @@
 import streamlit as st 
 import pandas as pd 
-import numpy as np
+import time 
+
 
 st.title('Layouts')
+
+side_bar = st.sidebar
+
+side_bar.header('sidebar')
+side_bar.write('this it sidebar')
+
+st.write('This is page')
+
+# load df 
+df = pd.read_csv('tips.csv')
 
