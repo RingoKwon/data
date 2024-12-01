c65b9e44c81482891b463b6dca1d9020cc8e2c9d
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-12-01 22:19:59 +0900

    save

---
+++ streamlit_study/2_api/basicStElements.py
@@ -0,0 +1,54 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+
+
+st.title("title")
+st.caption("explain caption ")
+st.header("header")
+st.subheader("subheader")
+
+df = pd.DataFrame({
+    'first_col' : [1,2,3], 
+    'second_col' : [1,2,3]
+})
+st.markdown("""
+# df 
+- see this 
+---
+""")
+st.write(df)
+
+body = '''
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+
+
+st.title("title")
+st.caption("explain caption ")
+st.header("header")
+st.subheader("subheader")
+
+df = pd.DataFrame({
+    'first_col' : [1,2,3], 
+    'second_col' : [1,2,3]
+})
+st.markdown("""
+# df 
+- see this 
+---
+""")
+st.write(df)
+'''
+
+st.subheader('codeblock')
+st.code(body, language='python')
+
+formula = """
+a+ar +a r^3 +\cdots + a r^{n-1} = \sum_{k=1}^{n-1} a r^k
+"""
+
+st.subheader('LaTex')
+st.latex(formula)
+
