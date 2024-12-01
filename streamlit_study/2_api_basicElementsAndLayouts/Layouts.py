df072c05cd617536fb47c30fca88930b98928ff9
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-12-02 00:16:15 +0900

    highlight

--- streamlit_study/2_api_basicElementsAndLayouts/Layouts.py
+++ streamlit_study/2_api_basicElementsAndLayouts/Layouts.py
@@ -14,4 +14,15 @@ st.write('This is page')
 
 # load df 
 df = pd.read_csv('tips.csv')
+st.dataframe(df)
 
+columns = tuple(df.columns)
+st.write(columns)
+
+#  create widget select box 
+select_column = side_bar.selectbox(
+    'Select the column you wnat to display', 
+    columns
+)
+
+side_bar.write('You selected the column_name = `{}'.format(select_column) + '`')
