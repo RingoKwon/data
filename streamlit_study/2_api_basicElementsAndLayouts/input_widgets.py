a1a1847a705e6a2fd76f733c57c7f221208f31ab
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-12-04 14:18:20 +0900

    radio

--- streamlit_study/2_api_basicElementsAndLayouts/input_widgets.py
+++ streamlit_study/2_api_basicElementsAndLayouts/input_widgets.py
@@ -19,5 +19,38 @@ click = st.button('sample data')
 if click:
     st.write(sample_function(data))
 
+# checkbox
+st.markdown('---')
+st.header('checkbox')
+st.subheader('single checkbox')
+checkbox = st.checkbox('show/hide')
+st.write("checkbox", checkbox)
+
+st.subheader('multiple checkbox')
+
+
+with st.container():
+    st.info('multiple checkbox')
+    checkbox1 = st.checkbox('checkbox1')
+    checkbox2 = st.checkbox('checkbox2')
+    checkbox3 = st.checkbox('checkbox3')
+    checkbox4 = st.checkbox('checkbox4')
+
+    button = st.button('submit')
+
+    if button:
+        dict = {'checkbox1': checkbox1,
+                'checkbox2': checkbox2, 
+                'checkbox3': checkbox3, 
+                'checkbox4': checkbox4  }
+        st.json(dict)
+
+# radio 
+st.markdown('---')
+st.header('radio')
+
+st.subheader('single radio')
+radio = st.radio('select', ['option1', 'option2', 'option3'])
+st.markdown(f'Selected: `{radio}`')
 
 
