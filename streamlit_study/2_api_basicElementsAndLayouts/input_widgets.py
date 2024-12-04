125e8556f844591419f1d0dfe3945c3f5ad0294d
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-12-04 13:52:05 +0900

    display random sample

--- streamlit_study/2_api_basicElementsAndLayouts/input_widgets.py
+++ streamlit_study/2_api_basicElementsAndLayouts/input_widgets.py
@@ -4,11 +4,20 @@ import numpy as np
 import os 
 
 data = pd.read_csv('tips.csv')
-st.write(data.head())
+
+def sample_function(df):
+    return df.sample(5)
+
 
 # button 
-click = st.button('click me')
-st.write('clicked', click)
+st.header('button')
+st.subheader('sample data')
+st.caption('click the button to see the sample data')
+ 
+click = st.button('sample data')
+
+if click:
+    st.write(sample_function(data))
 
 
 
